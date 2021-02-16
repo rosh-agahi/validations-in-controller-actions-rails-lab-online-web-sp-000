e5cbbc23d83cba684fe612d885154d36231c6be1
Roshanak Agahi <rosh.agahi@gmail.com>
2021-02-16 02:34:36 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,8 +8,19 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+      if @post.valid?
+            # If--and only if--the post is valid, do what we usually do.
+            @post.save
+            # This returns a status_code of 302, which instructs the browser to
+            # perform a NEW REQUEST! (AKA: throw @post away and let the show action
+            # worry about re-reading it from the database)
+            redirect_to post_path(@post)
+          else
+            # If the post is invalid, hold on to @post, because it is now full of
+            # useful error messages, and re-render the :new page, which knows how
+            # to display them alongside the user's entries.
+            render :new
+          end
   end
 
   private
