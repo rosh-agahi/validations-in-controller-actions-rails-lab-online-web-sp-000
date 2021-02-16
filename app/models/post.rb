6cb7c1ee9111b6ca35a39a8de21ab2ae28702248
Roshanak Agahi <rosh.agahi@gmail.com>
2021-02-16 02:39:11 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   
-  validates :name, presence: true
+  validates :title, presence: true 
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   
 end
